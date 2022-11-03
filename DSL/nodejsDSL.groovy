0d69747a9cd67eabefcdf2e5e76a224d69800299
Damian Baldi <114240668+damianbaldi@users.noreply.github.com>
2022-11-03 16:24:47 -0300

    Update nodejsDSL.groovy
    cambio repositorio

--- DSL/nodejsDSL.groovy
+++ DSL/nodejsDSL.groovy
@@ -1,9 +1,9 @@
 job('Aplicacion Node.js DSL') {
     description('Aplicación Node JS DSL para el curso de Jenkins')
     scm {
-        git('https://github.com/macloujulian/nodejsapp.git', 'master') { node ->
-            node / gitConfigName('macloujulian')
-            node / gitConfigEmail('macloujulian@gmail.com')
+        git('https://github.com/damianbaldi/nodejsapp.git', 'master') { node ->
+            node / gitConfigName('damianbaldi')
+            node / gitConfigEmail('dbaldi@teco.com.ar')
         }
     }
     triggers {
